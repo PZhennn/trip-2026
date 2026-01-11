9f253696fc352eac9f7e234f3d1cb6ce4a948d4c
PZhennn <nc901228@gmail.com>
2026-01-11 16:42:35 +0800

    Add files via upload
    Initial commit

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'aomori-v1';
+const URLS = [
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/vue@3/dist/vue.global.js',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
